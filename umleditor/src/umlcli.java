5ca3a070927546be2e9afbac4e747f5c98b39a3f
Zach VanGorden <70124634+zsvangor@users.noreply.github.com>
2022-02-07 02:25:28 -0500

    added basic skeleton for save load
    reformatted help command for increased clarity

--- umleditor/src/umlcli.java
+++ umleditor/src/umlcli.java
@@ -10,7 +10,7 @@ public class umlcli {
 	// "What would you like to add?""
 	// > class
 	// Then prompt for names and call the associated method on our UMLDiagram object
-	public static String[] commands = new String [] {"add", "rename", "delete", "help", "list", "exit"};
+	public static String[] commands = new String [] {"add", "rename", "delete", "help", "save", "load", "list", "exit"};
 	public static Scanner input = new Scanner (System.in);
 	public static UMLDiagram umld = new UMLDiagram();
 	public static boolean hasUnsavedWork = false;
@@ -18,21 +18,18 @@ public class umlcli {
 	
 	//Driver for the cli class
 	public static void main (String[] args) {
-		
 		prompt();
-
 		while (true) {
-			System.out.print("> ");
-			String command = input.nextLine();
+			System.out.println("Please enter a command:");
+			String command = getInput();
 			//if command not valid keep prompting for new one 
 			while (!isCommand(command)) {
-				System.out.println("Command not recognized.");
-				System.out.print("> ");
-				command = input.nextLine();
+				System.out.println("Command not recognized. Type 'help' for list of valid commands.");
+				System.out.println("Please enter a command:");
+				command = getInput();
 			}
 			runCommand(command);
 		}	
-		
 	}	
 	
 	//cli functions 
@@ -40,10 +37,9 @@ public class umlcli {
 	//Initial prompt 
 	public static void prompt () {
 		System.out.println("Welcome to the UML editor.");
-		System.out.println("Please enter a command.");
 		System.out.println("Type 'help' for list of valid commands.");
 		System.out.println("Type 'exit' to quit the editor.");
-  }
+  	}
   
 	public static String getInput () {
 		System.out.print("> ");
@@ -51,25 +47,22 @@ public class umlcli {
 	}
 	
 	//verify if word entered is a command
-		public static boolean isCommand (String command) {
-			for (String ele : commands)
-			{
-				if (command.equals(ele)) {
-					return true;
-				}
+	public static boolean isCommand (String command) {
+		for (String ele : commands) {
+			if (command.equals(ele)) {
+				return true;
 			}
-			return false;
 		}
+		return false;
+	}
 	
 	//exit the program if user intended to 
-	public static void exitCommand ()
-	{
+	public static void exitCommand () {
 		// if user hasn't saved since last change, inform them and give them a chance to not exit
 		if(hasUnsavedWork) {
 			System.out.print("There is currently unsaved work. ");
 		}
-		System.out.println("Are you sure you want to exit? (y/n)");
-    System.out.print("> ");
+		System.out.println("Are you sure you want to exit? [y/n]");
 		String answer = getInput();
 		// if "y", exit program
 		if(answer.equals("y")) {
@@ -87,16 +80,15 @@ public class umlcli {
 		System.out.println("With this UML editor, you can create a UML diagram.");
 		System.out.println("You can create classes with attributes and define relationships between those classes.");
 		System.out.println("Have fun with your UML project!");
-		listCommands();
-	}
-	
-	//list commands 
-	public static void listCommands () {
-		System.out.print("List of commands: ");
-		for (String ele : commands) {
-			System.out.print(" " + ele + " ,");
-		}
-		System.out.println();
+		System.out.println("List of commands: ");
+		System.out.println("\tadd: Add a new class or relationship to the diagram, or add an attribute to an existing class.");
+		System.out.println("\trename: Rename an existing class, relationship, or attribute.");
+		System.out.println("\tdelete: Delete an existing class, relationship, or attribute.");
+		System.out.println("\tlist: List all classes in the diagram, all relationships, or one specific class.");
+		System.out.println("\tsave: Save the current UML diagram to a JSON or YAML file.");
+		System.out.println("\tload: Load an existing UML diagram from a JSON or YAML file.");
+		System.out.println("\thelp: View all editor commands.");
+		System.out.println("\texit: Quit the editor.");
 	}
 	
 	//run given command 
@@ -111,32 +103,38 @@ public class umlcli {
 			addCommand();			
 		}
 		else if(command.equals("delete")) {
-			// needs implementation
+			deleteCommand();
 		}
 		else if(command.equals("rename")) {
-			// needs implementation
+			renameCommand();
 		}
 		else if(command.equals("list")) {
 			listCommand();
 		}
+		else if(command.equals("save")) {
+			saveDiagram();
+		}
+		else if(command.equals("load")) {
+			loadDiagram();
+		}
 		else {
-			System.out.println("Command not recognized.");
+			System.out.println("Command not recognized. Type 'help' for list of valid commands.");
 			// might be redundant
 		}	
 	}	
 	
 	public static void addCommand () {
-		System.out.println("What do you want to add?");
+		System.out.println("What do you want to add? [class/relationship/attribute]");
 		String toAdd = getInput();
 		if (toAdd.equals("class")) {
-			System.out.println("Enter Class name");
+			System.out.println("Enter class name: ");
 			String className = getInput();
 			umld.addClass(className);
 		}
 	}
 	
 	public static void listCommand () {
-		System.out.println("What do you want to list");
+		System.out.println("What do you want to list? [class/classes/relationships]");
 		String toList = getInput();
 		if (toList.equals("classes")) {
 			listClasses();
@@ -146,10 +144,18 @@ public class umlcli {
 			listClass();
 		}
 		else {
-			System.out.println("Command not recognized.");
-			// might be redundant
+			System.out.println("Command not recognized. Type 'help' for list of valid commands.");
 		}
-		
+	}
+
+	public static void renameCommand() {
+		System.out.println("What do you want to rename? [class/relationship/attribute]");
+		// needs implementation
+	}
+
+	public static void deleteCommand() {
+		System.out.println("What do you want to delete? [class/relationship/attribute]");
+		// needs implementation
 	}
 	
 	//list all classes in the diagram 
@@ -171,10 +177,10 @@ public class umlcli {
 	//need to implement (not finished)
 
 	public static void listClass () {
-		System.out.println("Enter class name");
+		System.out.println("Enter class name: ");
 		String className = getInput();
 		Iterator hmIter = umld.umlDiagram.entrySet().iterator();
-		while (hmIter .hasNext()) {
+		while (hmIter.hasNext()) {
 			Map.Entry mapElem = (Map.Entry) hmIter.next();
 			if (mapElem.getKey().equals(className))
 			{
@@ -184,6 +190,14 @@ public class umlcli {
 			}			
 		}
 		System.out.println("Class '" + className + "' does not exist.");
+	}
+
+	public static void saveDiagram() {
+		// needs implementation
+	}
+
+	public static void loadDiagram() {
+		// needs implementation
 	}
 	
 }
