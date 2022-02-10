a454d5461d16d98c94baee1e8b1754dc1d042024
Matthew North <mjnorth@millersville.edu>
2022-02-09 23:33:43 -0500

    Added a class to represent a relationship

---
+++ umleditor/src/UMLRelationship.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+
+/**
+ * This class represents a relationship between two classes in a UML diagram.
+ * It consists of a constructor and getter and setter methods for the class's data.
+ */
+
+public class UMLRelationship
+{
+    /**
+     * Class data. This is accessed using getter and setter methods below.
+     */
+    private String src, dest, type;
+
+    /**
+     * Valid relationship types
+     */
+    private static final String[] validTypes = ["Nondirectional"];
+
+    /**
+     * Constructor for a relationship in a UML diagram
+     * @param relSrc The source class for the relationship
+     * @param relDest The destination class for the relationship
+     * @param relType The type of this relationship. Must be: Nondirectional
+     */
+    public UMLRelationship(String relSrc, String relDest, String relType)
+    {
+        src = relSrc;
+        dest = relDest;
+        type = relType;
+    }
+
+    /////////////////////////////////////////////////////////////
+
+    /**
+     * Returns the source class of this relationship
+     * @return This relationship's source class
+     */
+    public String getSource()
+    {
+        return src;
+    }
+
+    /**
+     * Sets the source class of this relationship
+     * @param newSrc New source class for this relationship
+     */
+    public void setSource(String newSrc)
+    {
+        src = newSrc;
+    }
+
+    /////////////////////////////////////////////////////////////
+
+    /**
+     * Returns the destination class of this relationship
+     * @return This relationship's destination class
+     */
+    public String getDestination()
+    {
+        return dest;
+    }
+
+    /**
+     * Sets the destination class of this relationship
+     * @param newDest New destination class for this relationship
+     */
+    public void setDestination(String newDest)
+    {
+        dest = newDest;
+    }
+
+    /////////////////////////////////////////////////////////////
+
+    /**
+     * Returns the type of this relationship
+     * @return This relationship's type
+     */
+    public String getType()
+    {
+        return type;
+    }
+
+    /**
+     * Sets the type of this relationship
+     * @param newType New type for this relationship
+     */
+    public void setType(String newType)
+    {
+        type = newType;
+    }
+
+    /**
+     * Helper method to determine if a relationship type is valid
+     * @param relType The relationship type being checked
+     * @return True if the relationship type is valid, false if it's not
+     */
+    private boolean isValidType(String relType)
+    {
+        // Iterate through array containing valid types
+        for(String elem : UMLRelationship.validTypes)
+        {
+            if(relType == elem)
+            {
+                // Type matches a valid type in the array
+                return true;
+            }
+        }
+        // Reached end of array
+        return false;
+    }
+}
