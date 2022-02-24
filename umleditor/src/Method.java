a103088418094621b3a20ee9af259449eb9b6396
abalagurak <alexanderbalagurak@gmail.com>
2022-02-24 01:08:00 -0500

    added parameter support in methods class

--- umleditor/src/Method.java
+++ umleditor/src/Method.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 
 public class Method {
 
     private String methodName;
+    private ArrayList<Parameter> parameters = new ArrayList<Parameter>();
 
     /**
      * Creates a new method.
@@ -25,6 +27,59 @@ public class Method {
      */
     public void renameMethod(String newName){
         this.methodName = newName;
+    }
+
+    /**
+     * Adds a new parameter to the method.
+     * @param paramName
+     */
+    public void addParameter(String paramName){
+        parameters.add(new Parameter(paramName));
+    }
+
+    /**
+     * Removes a parameter from the method if it exists.
+     * @param paramName
+     */
+    public void removeParameter(String paramName){
+        for(int i = 0; i < parameters.size(); i++){
+            if(parameters.get(i).getParamName().equals(paramName)){
+                parameters.remove(i);
+            }
+        }
+    }
+
+    /**
+     * Removes all parameters from the method.
+     */
+    public void removeAllParameters(){
+        parameters.clear();
+    }
+
+    /**
+     * Renames a parameter in the method if it exists.
+     * @param oldParamName
+     * @param newParamName
+     */
+    public void renameParameter(String oldParamName, String newParamName){
+        for(int i = 0; i < parameters.size(); i++){
+            if(parameters.get(i).getParamName().equals(oldParamName)){
+                getParameter(oldParamName).renameParam(newParamName);
+            }
+        }
+    }
+
+    /**
+     * Returns a parameter if it exists.
+     * @param paramName
+     */
+    public Parameter getParameter(String paramName){
+        for(int i = 0; i < parameters.size(); i++){
+            if(parameters.get(i).getParamName().equals(paramName)){
+                return parameters.get(i);
+            }
+        }
+        return null;
     }
 
 }
