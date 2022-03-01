b278b36ce10a6c942545a6c324bbc3b81904d807
Issac E Rifa <44537605+issacjrr@users.noreply.github.com>
2022-02-28 21:24:50 -0500

    added menu for rest of components

--- umleditor/src/GUIView.java
+++ umleditor/src/GUIView.java
@@ -15,7 +15,6 @@ class GUIView implements ActionListener
     static JLabel text;
 	static JFrame main = new JFrame("UMLEditor");
 	static UMLDiagram umld = new UMLDiagram();
-	static Graphics update;
 	static final int WIDTH = 150;
 	static final int HEIGHT = 125;
 	//can have up to 6 classes across by up to 4 classes down
@@ -34,27 +33,124 @@ class GUIView implements ActionListener
     	
     	//Create an object
     	GUIView obj = new GUIView();
-    	//Create a Menu
+    	
+    	//Create Classes Menu
     	JMenu classes = new JMenu("Classes");
 		
-    	//Create Menu Items
-    	JMenuItem item[] = new JMenuItem[5];
-    	item [0] = new JMenuItem ("Add");
-    	item [1] = new JMenuItem ("Rename");
-    	item [2] = new JMenuItem ("Delete");
-    	item [3] = new JMenuItem ("List class");
-    	item [4] = new JMenuItem ("List all");
-    	for(int i=0;i<5;i++)
-    	{
-    		item[i].addActionListener(obj);
-    		classes.add(item[i]);
+    	//Create Menu Items for classes
+    	JMenuItem[] itemC = new JMenuItem[5];
+    	itemC [0] = new JMenuItem ("Add class");
+    	itemC [1] = new JMenuItem ("Rename class");
+    	itemC [2] = new JMenuItem ("Delete class");
+    	itemC [3] = new JMenuItem ("List class");
+    	itemC [4] = new JMenuItem ("List all classes");
+    	for (int i = 0; i < itemC.length; ++i) {
+    		itemC[i].addActionListener(obj);
+    		classes.add(itemC[i]);
     	}
     
+    	
+    	//Create Fields Menu
+    	JMenu fields = new JMenu("Fields");
+    	
+    	//Create Menu Items for fields
+    	JMenuItem[] itemF = new JMenuItem[5];
+    	itemF [0] = new JMenuItem ("Add field");
+    	itemF [1] = new JMenuItem ("Rename field");
+    	itemF [2] = new JMenuItem ("Delete field");
+    	itemF [3] = new JMenuItem ("List field");
+    	itemF [4] = new JMenuItem ("List all fields");
+    	for (int i = 0; i < itemF.length ; ++i) {
+    		itemF[i].addActionListener(obj);
+    		fields.add(itemF[i]);
+    	}
+    	
+    	
+    	//Create Methods Menu
+    	JMenu methods = new JMenu("Methods");
+    	
+    	//Create Menu Items for methods
+    	JMenuItem[] itemM = new JMenuItem[5];
+    	itemM [0] = new JMenuItem ("Add method");
+    	itemM [1] = new JMenuItem ("Rename method");
+    	itemM [2] = new JMenuItem ("Delete method");
+    	itemM [3] = new JMenuItem ("List method");
+    	itemM [4] = new JMenuItem ("List all methods");
+    	for (int i = 0; i < itemM.length; ++i) {
+    		itemM[i].addActionListener(obj);
+    		methods.add(itemM[i]);
+    	}
+    	
+    	
+    	//Create parameters Menu
+    	JMenu parameters = new JMenu("Parameters");
+    	
+    	//Create Menu Items for parameters
+    	JMenuItem[] itemP = new JMenuItem[6];
+    	itemP [0] = new JMenuItem ("Add parameter");
+    	itemP [1] = new JMenuItem ("Add parameters");
+    	itemP [2] = new JMenuItem ("Remove parameter");
+    	itemP [3] = new JMenuItem ("Remove parameters");
+    	itemP [4] = new JMenuItem ("Change parameter");
+    	itemP [5] = new JMenuItem ("Change parameters");
+    	for (int i = 0; i < itemP.length; ++i) { 
+    		itemP[i].addActionListener(obj);
+    		parameters.add(itemP[i]);
+    	}
+    	
+    	
+    	//Create File Menu
+    	JMenu file = new JMenu("File");
+    	
+    	//Create Menu Items for file
+    	JMenuItem[] itemFi = new JMenuItem[2];
+    	itemFi [0] = new JMenuItem ("Save");
+    	itemFi [1] = new JMenuItem ("Load");
+    	for (int i = 0; i < itemFi.length; ++i) {
+    		itemFi[i].addActionListener(obj);
+    		file.add(itemFi[i]);
+    	}
+    	
+    	
+    	//Create relationships menu
+    	JMenu relationship = new JMenu("Relationships");
+    	
+    	//Create Menu Items for relationships
+    	JMenuItem[] itemR = new JMenuItem[5];
+    	itemR [0] = new JMenuItem ("Add relationship");
+    	itemR [1] = new JMenuItem ("Change relationship");
+    	itemR [2] = new JMenuItem ("Delete relationship");
+    	itemR [3] = new JMenuItem ("List relationship");
+    	itemR [4] = new JMenuItem ("List all relationships");
+    	for (int i = 0; i < itemR.length; ++i) {
+    		itemR[i].addActionListener(obj);
+    		relationship.add(itemR[i]);
+    	}
+    	
+    	
+    	//Create program menu
+    	JMenu editor = new JMenu ("Editor");
+    	
+    	//Create menu items for program menu
+    	JMenuItem[] itemE = new JMenuItem[2];
+    	itemE [0] = new JMenuItem ("Help");
+    	itemE [1] = new JMenuItem ("Run on cli mode");
+    	for (int i = 0; i < itemE.length; ++i) {
+    		itemE[i].addActionListener(obj);
+    		editor.add(itemE[i]);
+    	}
+    	
     	//Create a menu bar
     	JMenuBar mb=new JMenuBar();
-    	mb.add(classes);
     	main.setJMenuBar(mb);
-	
+    	mb.add(file);
+    	mb.add(classes);
+    	mb.add(fields);
+    	mb.add(methods);
+    	mb.add(parameters);
+    	mb.add(relationship);
+    	mb.add(editor);
+    	
     	//Create the label
     	text = new JLabel();
     	main.add(text);
@@ -63,11 +159,11 @@ class GUIView implements ActionListener
     	main.setVisible(true);
     }
     
-    //Function to display the menu item selected
+    //Function to implement menu items 
     public void actionPerformed(ActionEvent e)
     {    	
     	
-    	if (e.getActionCommand().equals("Add")) {
+    	if (e.getActionCommand().equals("Add class")) {
     		String className = JOptionPane.showInputDialog(main, "Enter class name");
     		if (!umld.classExists(className)) {
     		umld.addClass(className);
@@ -79,7 +175,7 @@ class GUIView implements ActionListener
     			text.setText("class already exists, try again");
     		}
     
-    	} else if (e.getActionCommand().equals("Rename")) {
+    	} else if (e.getActionCommand().equals("Rename class")) {
     		String oldName = JOptionPane.showInputDialog (main, "Enter class to rename");
     		int localIndex = findIndex(oldName);
     		if (localIndex != -1 ) {
@@ -87,26 +183,28 @@ class GUIView implements ActionListener
     			umld.renameClass(oldName, newName);
     			classNames[localIndex] = newName;
     			Graphics classAdded = main.getGraphics();
-    			classAdded.clearRect(classRep.get(localIndex).x, classRep.get(localIndex).y - 20, 50, 19);
-    			classAdded.drawString (classNames[localIndex], classRep.get(localIndex).x + 20 ,classRep.get(localIndex).y);
+    			classAdded.clearRect(classRep.get(localIndex).x, classRep.get(localIndex).y - 20, 125, 19);
+    			classAdded.drawString (classNames[localIndex], classRep.get(localIndex).x + 20 ,classRep.get(localIndex).y - 5);
     		} else {
     			JOptionPane.showMessageDialog(main, "Class not found");
     		}
     		updateFirstRow();
-    	} else if (e.getActionCommand().equals("Delete")) {
+    		//need to finish
+    	} else if (e.getActionCommand().equals("Delete class")) {
     		String className = JOptionPane.showInputDialog (main, "Enter class to delete");
     		umld.removeClass(className);
     		
-    		update.clearRect(25, 70, 160, 160);
-    		updateFirstRow();
+    		
     	} else if (e.getActionCommand().equals("List class")) {
     		String className = JOptionPane.showInputDialog(main, "Enter class name");
     		JOptionPane.showMessageDialog(main, listClass(className));
     		updateFirstRow();
-    	} else if (e.getActionCommand().equals("List all")) {
+    	} else if (e.getActionCommand().equals("List all classes")) {
     		JOptionPane.showMessageDialog(main, listOfClasses());
     		updateFirstRow();
-    	} 
+    	} else {
+    		text.setText("Still working on it");
+    	}
     	
     }
     
@@ -153,7 +251,7 @@ class GUIView implements ActionListener
     	classNames[index] = className;
     	Graphics classAdded = main.getGraphics();
 		classAdded.drawRect(classRep.get(index).x, classRep.get(index).y, WIDTH, HEIGHT);
-		classAdded.drawString(className, myComp[0] + 20, myComp[1]);
+		classAdded.drawString(className, myComp[0] + 20, myComp[1] - 5);
 		++ index;
     }
     
@@ -178,7 +276,7 @@ class GUIView implements ActionListener
     		if (i % 4 == 0) {
     			Graphics classAdded = main.getGraphics();
     			classAdded.drawRect(classRep.get(i).x, classRep.get(i).y, WIDTH, HEIGHT);
-    			classAdded.drawString (classNames[i], classRep.get(i).x + 20 ,classRep.get(i).y);
+    			classAdded.drawString (classNames[i], classRep.get(i).x + 20 ,classRep.get(i).y - 5);
     		}
     	}
     }
