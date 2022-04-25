692531af4467109765542cb481e943f41cb7aebe
ncmonser <ncmonser@millersville.edu>
2022-04-24 20:09:37 -0400

    Tests for UMLDiagram

--- lib/src/test/java/TemporaryGroupGradle/TestUMLDiagram.java
+++ lib/src/test/java/TemporaryGroupGradle/TestUMLDiagram.java
@@ -34,5 +34,133 @@ public class TestUMLDiagram {
         umld.redo();
         assertFalse(umld.canRedo());
     }
+    
+    @Test
+    void testClass() {
+    	UMLDiagram umld = new UMLDiagram();
+    	assertTrue(umld.addClass("Car"));
+    }
+    
+    @Test
+    void removeClass() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	assertTrue(umld.removeClass("Car"));
+    }
+    
+    @Test
+    void renameClass() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	assertTrue(umld.renameClass("Car", "NewCar"));
+    }
+    
+    @Test
+    void Relationships() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addClass("Road");
+    	umld.addRelationship("Car", "Road", "aggregation");
+    	assertTrue(umld.deleteRelationship("Car", "Road"));
+    }
+    
+    @Test
+    void removeParam() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addClass("Road");
+    	umld.addMethod("Car", "CarMethod", "void");
+    	umld.addParameter("Car", "CarMethod", "CarParam", "string");
+    	assertTrue(umld.removeParameter("Car", "CarMethod", "CarParam"));
+    }
+    
+    @Test
+    void removeNullParam() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addMethod("Car", "CarMethod", "void");
+    	assertFalse(umld.removeParameter("Car", "CarMethod", "NullParam"));
+    }
+    
+    @Test
+    void removeAllParam() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addClass("Road");
+    	umld.addMethod("Car", "CarMethod", "void");
+    	umld.addParameter("Car", "CarMethod", "CarParam", "string");
+    	umld.addParameter("Car", "CarMethod", "CarParam2", "string");
+    	assertTrue(umld.removeAllParameters("Car","CarMethod"));
+    }
+    
+    @Test
+    void renameParameter() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addClass("Road");
+    	umld.addMethod("Car", "CarMethod", "void");
+    	umld.addParameter("Car", "CarMethod", "CarParam", "string");
+    	assertTrue(umld.renameParameter("Car", "CarMethod", "CarParam", "CarNewParam"));
+    }
+    
+    @Test
+    void removeMethod() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addClass("Road");
+    	umld.addMethod("Car", "CarMethod", "void");
+    	assertTrue(umld.removeMethod("Car", "CarMethod"));
+    }
+    
+    @Test
+    void renameMethod() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addClass("Road");
+    	umld.addMethod("Car", "CarMethod", "void");
+    	assertTrue(umld.renameMethod("Car", "CarMethod", "CarNewMethod"));
+    }
+    
+    @Test
+    void renameMethodType() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addClass("Road");
+    	umld.addMethod("Car", "CarMethod", "void");
+    	assertTrue(umld.renameMethodType("Car", "CarMethod", "void"));
+    }
+    
+    @Test
+    void addFields() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	assertTrue(umld.addField("Car", "CarField", "string"));
+    }
+    
+    @Test
+    void removeFields() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addField("Car", "CarField", "string");
+    	assertTrue(umld.removeField("Car","CarField"));
+    }
+   
+    @Test 
+    void renameFields() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addField("Car", "CarField", "string");	
+    	assertTrue(umld.renameField("Car","CarField", "CarNewField"));
+    }
+    
+    @Test
+    void renameFieldType() {
+    	UMLDiagram umld = new UMLDiagram();
+    	umld.addClass("Car");
+    	umld.addField("Car", "CarField", "string");
+    	assertTrue(umld.renameFieldType("Car", "CarField", "int"));
+    }
+    
+   
 
 }
